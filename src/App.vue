10752e69c920a65cfccb6fe700d94498ef450595
ItsKristian01 <kristianhana11@gmail.com>
2024-02-01 17:07:09 +0100

    add: ProjectCard component and imported to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
+
 
 export default {
   data() {
     return {
       baseUrl: 'http://127.0.0.1:8000',
+      projects: [],
     }
   },
   created() {
     axios.get(`${this.baseUrl}/api/projects`).then((resp) => {
-      console.log(resp);
+      // console.log(resp);
+      this.projects = resp.data.result;
     })
-  }
+  },
+  components: { ProjectCard }
 }
 </script>
 
 <template>
-<h1>TEST</h1>
+  <div class="container">
+    <h2 class="text-center mt-2">List of all Projects</h2>
+    <div class="row row-cols-3 p-3 g-4">
+      <div v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
