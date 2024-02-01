ab08dd8c375b35e1b374f14552f543858ce07785
ItsKristian01 <kristianhana11@gmail.com>
2024-02-01 16:21:27 +0100

    add: added call api with axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-
+      baseUrl: 'http://127.0.0.1:8000',
     }
+  },
+  created() {
+    axios.get(`${this.baseUrl}/api/projects`).then((resp) => {
+      console.log(resp);
+    })
   }
 }
 </script>
