0f8776b77d4d17804cc4f5ef25895056da3d7de3
ItsKristian01 <kristianhana11@gmail.com>
2024-02-02 16:24:10 +0100

    add: router-view

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,21 @@
 <script>
 import axios from 'axios';
-import ProjectCard from './components/ProjectCard.vue';
-import AppHeader from './components/AppHeader.vue'
+import AppHeader from './components/AppHeader.vue';
 
 
 export default {
   data() {
     return {
-      baseUrl: 'http://127.0.0.1:8000',
-      projects: [],
     }
   },
-  created() {
-    axios.get(`${this.baseUrl}/api/projects`).then((resp) => {
-      // console.log(resp);
-      this.projects = resp.data.result;
-    })
-  },
-  components: { ProjectCard, AppHeader}
+  components: {AppHeader}
 }
 </script>
 
 <template>
   <AppHeader />
-
-  <div class="container">
-    <h2 class="text-center mt-2">List of all Projects</h2>
-    <div class="row row-cols-3 p-3 g-4">
-      <div v-for="project in projects" :key="project.id">
-        <ProjectCard :project="project" />
-      </div>
-    </div>
-  </div>
+  <router-view></router-view>
+ 
 </template>
 
 <style lang="scss">
