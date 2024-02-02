ab5cb986badfb07513df3ba07cafa92bbfc22812
ItsKristian01 <kristianhana11@gmail.com>
2024-02-02 16:24:34 +0100

    add: pages

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="container mt-5">
+       <h2>About us</h2>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      baseUrl: 'http://127.0.0.1:8000',
+      projects: [],
+    }
+  },
+  created() {
+    axios.get(`${this.baseUrl}/api/projects`).then((resp) => {
+      // console.log(resp);
+      this.projects = resp.data.result;
+    })
+  },
+  components: { ProjectCard}
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="text-center mt-2">List of all Projects</h2>
+        <div class="row row-cols-3 p-3 g-4">
+            <div v-for="project in projects" :key="project.id">
+                <div class="card h-100 p-2">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.title }}</h5>
+                        <p class="card-text">{{ project.description }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
