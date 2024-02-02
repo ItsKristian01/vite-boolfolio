7d5b3d1938f63f13643b303646391478989d17f2
ItsKristian01 <kristianhana11@gmail.com>
2024-02-02 16:52:51 +0100

    add: router.js

--- src/router.js
+++ src/router.js
@@ -1,22 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HomePage from './pages/HomePage.vue';
-import AboutPage from './pages/AboutPage.vue';
+import HomePage from "./pages/HomePage.vue";
+import AboutPage from "./pages/AboutPage.vue";
 
 const router = createRouter({
-    history: createWebHistory({
-        route: [
-            {
-                path: '/',
-                name: 'home',
-                component: HomePage
-            },
-            {
-                path: '/',
-                name: 'about',
-                component: AboutPage
-            },
-        ]
-    })
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutPage,
+    },
+  ],
 });
 export { router };
