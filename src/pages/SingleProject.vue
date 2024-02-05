d41922c8498562cf9d488d8d9201037993d2f3b2
ItsKristian01 <kristianhana11@gmail.com>
2024-02-05 17:06:14 +0100

    add: SingleProject components on pages

---
+++ src/pages/SingleProject.vue
@@ -0,0 +1,22 @@
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+        console.log(this.$route.params.slug)
+    }
+};
+</script>
+
+
+<template>
+    <div class="container mt-4">
+        <h2>Single Project</h2>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
