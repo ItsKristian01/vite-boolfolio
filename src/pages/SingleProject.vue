6d3f8b4f2f8ed188c87e75b70e2aa64b1d174718
ItsKristian01 <kristianhana11@gmail.com>
2024-02-05 17:11:43 +0100

    add: axios api call

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,13 +1,20 @@
 
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
-
+            baseUrl: 'http://127.0.0.1:8000',
         }
     },
     created() {
-        console.log(this.$route.params.slug)
+        // console.log(this.$route.params.slug)
+
+        axios.get(`${this.baseUrl}/api/projects/${ this.$route.params.slug }`).then((resp) => {
+            // console.log(resp);
+            this.projects = resp.data.result;
+        })
     }
 };
 </script>
