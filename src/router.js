d22b14180e9f1c625c314d045688014836b370e1
ItsKristian01 <kristianhana11@gmail.com>
2024-02-05 17:05:10 +0100

    add: SingleProject on router.js

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
 import AboutPage from "./pages/AboutPage.vue";
-import SingleProject from "./pages/AboutPage.vue";
+import SingleProject from './pages/SingleProject.vue';
 
 const router = createRouter({
   history: createWebHistory(),
