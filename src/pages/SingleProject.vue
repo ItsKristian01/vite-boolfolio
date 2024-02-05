b8f4e7a671cf0a16fbaf77b83d9921caa47228f9
ItsKristian01 <kristianhana11@gmail.com>
2024-02-05 17:23:37 +0100

    add: projects title from api

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -6,13 +6,14 @@ export default {
     data() {
         return {
             baseUrl: 'http://127.0.0.1:8000',
+            projects: [],
         }
     },
     created() {
         // console.log(this.$route.params.slug)
 
-        axios.get(`${this.baseUrl}/api/projects/${ this.$route.params.slug }`).then((resp) => {
-            // console.log(resp);
+        axios.get(`${this.baseUrl}/api/projects/${this.$route.params.slug}`).then((resp) => {
+            //console.log(resp);
             this.projects = resp.data.result;
         })
     }
@@ -21,8 +22,12 @@ export default {
 
 
 <template>
-    <div class="container mt-4">
-        <h2>Single Project</h2>
+    <div class="container">
+        <div class="card h-100 p-2 mt-5">
+            <div class="card-body">
+                <h5 class="card-title">{{ projects.title }}</h5>
+            </div>
+        </div>
     </div>
 </template>
 
